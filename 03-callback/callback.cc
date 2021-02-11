b1f8867bb270537d1f1f27cf6fffe804b7391c97
ideaspaper <agni.infinity@gmail.com>
2021-02-12 06:33:55 +0700

    fix: remove Napi namespace

--- 03-callback/callback.cc
+++ 03-callback/callback.cc
@@ -2,41 +2,39 @@
 #include <thread>
 #include <chrono>
 
-using namespace Napi;
-
-bool ArgumentsValidator(const CallbackInfo& info) {
-    Env env = info.Env();
+bool ArgumentsValidator(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
     if (info.Length() != 2) {
-        TypeError::New(env, "Wrong argument(s)").ThrowAsJavaScriptException();
+        Napi::TypeError::New(env, "Wrong argument(s)").ThrowAsJavaScriptException();
         return false;
     }
     if (!info[0].IsNumber() || !info[1].IsFunction()) {
-        TypeError::New(env, "Wrong argument(s)").ThrowAsJavaScriptException();
+        Napi::TypeError::New(env, "Wrong argument(s)").ThrowAsJavaScriptException();
         return false;
     }
-    if (info[0].As<Number>().Int32Value() < 0 || info[0].As<Number>().Int32Value() > 5) {
-        RangeError::New(env, "Argument(s) out of range").ThrowAsJavaScriptException();
+    if (info[0].As<Napi::Number>().Int32Value() < 0 || info[0].As<Napi::Number>().Int32Value() > 5) {
+        Napi::RangeError::New(env, "Argument(s) out of range").ThrowAsJavaScriptException();
         return false;
     }
     return true;
 }
 
-Value Callback(const CallbackInfo& info) {
-    Env env = info.Env();
+Napi::Value Callback(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
     if (!ArgumentsValidator(info)) return env.Null();
 
-    Function callback = info[1].As<Function>();
+    Napi::Function callback = info[1].As<Napi::Function>();
 
-    for (int i = 0; i < info[0].As<Number>().Int32Value(); i++) {
+    for (int i = 0; i < info[0].As<Napi::Number>().Int32Value(); i++) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
-    callback.Call(env.Global(), {String::New(env, "String from Addon - Hello World!")});
+    callback.Call(env.Global(), {Napi::String::New(env, "String from Addon - Hello World!")});
     return env.Null();
 }
 
-Object Init(Env env, Object exports) {
-    exports.Set(String::New(env, "Callback"), Function::New(env, Callback));
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
+    exports.Set(Napi::String::New(env, "Callback"), Napi::Function::New(env, Callback));
     return exports;
 }
 
