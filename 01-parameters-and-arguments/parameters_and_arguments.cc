a94e539532f6efbc46a92a56b9e678c9cc5a3e44
ideaspaper <agni.infinity@gmail.com>
2021-02-12 06:37:52 +0700

    fix: remove Napi namespace

--- 01-parameters-and-arguments/parameters_and_arguments.cc
+++ 01-parameters-and-arguments/parameters_and_arguments.cc
@@ -1,61 +1,59 @@
 #include <napi.h>
 
-using namespace Napi;
-
-bool ArgumentsValidator(const CallbackInfo& info) {
-    Env env = info.Env();
+bool ArgumentsValidator(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
     if (info.Length() != 2) {
-        TypeError::New(env, "Wrong argument(s)").ThrowAsJavaScriptException();
+        Napi::TypeError::New(env, "Wrong argument(s)").ThrowAsJavaScriptException();
         return false;
     };
     if (!info[0].IsNumber() || !info[1].IsNumber()) {
-        TypeError::New(env, "Wrong argument(s)").ThrowAsJavaScriptException();
+        Napi::TypeError::New(env, "Wrong argument(s)").ThrowAsJavaScriptException();
         return false;
     }
     return true;
 }
 
-Value Add(const CallbackInfo& info) {
-    Env env = info.Env();
+Napi::Value Add(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
     if (!ArgumentsValidator(info)) return env.Null();
 
-    double val1 = info[0].As<Number>().DoubleValue();
-    double val2 = info[1].As<Number>().DoubleValue();
-    return Number::New(env, val1 + val2);
+    double val1 = info[0].As<Napi::Number>().DoubleValue();
+    double val2 = info[1].As<Napi::Number>().DoubleValue();
+    return Napi::Number::New(env, val1 + val2);
 }
 
-Value Sub(const CallbackInfo& info) {
-    Env env = info.Env();
+Napi::Value Sub(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
     if (!ArgumentsValidator(info)) return env.Null();
 
-    double val1 = info[0].As<Number>().DoubleValue();
-    double val2 = info[1].As<Number>().DoubleValue();
-    return Number::New(env, val1 - val2);
+    double val1 = info[0].As<Napi::Number>().DoubleValue();
+    double val2 = info[1].As<Napi::Number>().DoubleValue();
+    return Napi::Number::New(env, val1 - val2);
 }
 
-Value Mul(const CallbackInfo& info) {
-    Env env = info.Env();
+Napi::Value Mul(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
     if (!ArgumentsValidator(info)) return env.Null();
 
-    double val1 = info[0].As<Number>().DoubleValue();
-    double val2 = info[1].As<Number>().DoubleValue();
-    return Number::New(env, val1 * val2);
+    double val1 = info[0].As<Napi::Number>().DoubleValue();
+    double val2 = info[1].As<Napi::Number>().DoubleValue();
+    return Napi::Number::New(env, val1 * val2);
 }
 
-Value Div(const CallbackInfo& info) {
-    Env env = info.Env();
+Napi::Value Div(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
     if (!ArgumentsValidator(info)) return env.Null();
 
-    double val1 = info[0].As<Number>().DoubleValue();
-    double val2 = info[1].As<Number>().DoubleValue();
-    return Number::New(env, val1 / val2);
+    double val1 = info[0].As<Napi::Number>().DoubleValue();
+    double val2 = info[1].As<Napi::Number>().DoubleValue();
+    return Napi::Number::New(env, val1 / val2);
 }
 
-Object Init(Env env, Object exports) {
-    exports.Set(String::New(env, "Add"), Function::New(env, Add));
-    exports.Set(String::New(env, "Sub"), Function::New(env, Sub));
-    exports.Set(String::New(env, "Mul"), Function::New(env, Mul));
-    exports.Set(String::New(env, "Div"), Function::New(env, Div));
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
+    exports.Set(Napi::String::New(env, "Add"), Napi::Function::New(env, Add));
+    exports.Set(Napi::String::New(env, "Sub"), Napi::Function::New(env, Sub));
+    exports.Set(Napi::String::New(env, "Mul"), Napi::Function::New(env, Mul));
+    exports.Set(Napi::String::New(env, "Div"), Napi::Function::New(env, Div));
     return exports;
 }
 
